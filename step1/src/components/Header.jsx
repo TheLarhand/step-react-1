fcfbb28e5f9679d6a6734a9b5d07af38ffbc7fe9
Игорь Соколов <@itstep.krg>
2024-03-15 17:32:15 +0500

    add some details

--- step1/src/components/Header.jsx
+++ step1/src/components/Header.jsx
@@ -1,13 +1,17 @@
+import classes from "./classes/Header.module.css";
 const Header = ({navLinks, books}) => {
   return (
   <header>
-    <div>
-      <h2>Библивотека</h2>
-      <nav>
-        {navLinks.map ((navLink) => 
-          <a href={navLink.href}>{navLink.title}</a>
-        )}
-      </nav>
+    <div className={classes.header_top}>
+      <div className={classes.navigation}>
+        <h2>Библивотека</h2>
+        <nav>
+          {navLinks.map ((navLink) => 
+            <a href={navLink.href}>{navLink.title}</a>
+          )}
+        </nav>
+      </div>
+      
     </div>
 
   <div>
